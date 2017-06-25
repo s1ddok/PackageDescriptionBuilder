ada776d014a383f64aaba393982a1ba96705937e
s1ddok <siddok@gmail.com>
2017-06-25 23:47:09 +0300

    Initial Commit

---
+++ PackageDescriptionBuilder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PackageDescriptionBuilder
+//
+//  Created by Andrey Volodin on 25.06.17.
+//  Copyright © 2017 s1ddok. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
