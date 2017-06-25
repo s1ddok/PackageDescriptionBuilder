8f8cf2805d2a1dcf86c34a5a222216446a879b12
s1ddok <siddok@gmail.com>
2017-06-25 23:52:55 +0300

    add autocompletion for PackageDescription

--- PackageDescriptionBuilder/main.swift
+++ PackageDescriptionBuilder/main.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2017 s1ddok. All rights reserved.
 //
 
-import Foundation
+import PackageDescription
 
-print("Hello, World!")
+// Start writing here the following:
+// "let package = Package(... "
+// then you will get the autocompletion
+// after you finish your manifest, you can simply copy it to your repo.
 
+// Enjoy!
